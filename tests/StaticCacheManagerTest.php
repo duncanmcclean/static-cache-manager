72ac3173ec593e35ea91094d90c8593faac4f886
Duncan McClean <duncan@duncanmcclean.com>
2024-06-08 16:12:20 +0100

    Add tests (#25)
    * Add tests
    
    * Delete users directory in fixtures

---
+++ tests/StaticCacheManagerTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace DuncanMcClean\StaticCacheManager\Tests;
+
+use Illuminate\Support\Facades\File;
+use PHPUnit\Framework\Attributes\Test;
+use Statamic\Facades\Config;
+use Statamic\Facades\User;
+
+class StaticCacheManagerTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        File::copyDirectory(__DIR__.'/__fixtures__/public/static', base_path('public/static'));
+    }
+
+    public function tearDown(): void
+    {
+        File::deleteDirectory(base_path('public/static'));
+
+        parent::tearDown();
+    }
+
+    #[Test] public function it_clears_a_statically_cached_page()
+    {
+        $this->assertFileExists(base_path('public/static/_.html'));
+        $this->assertFileExists(base_path('public/static/about_.html'));
+        $this->assertFileExists(base_path('public/static/contact_.html'));
+
+        $this
+            ->actingAs(User::make()->makeSuper()->save())
+            ->post(cp_route('utilities.static-cache-manager.clear'), [
+                'paths' => '/about',
+            ]);
+
+        $this->assertFileExists(base_path('public/static/_.html'));
+        $this->assertFileExists(base_path('public/static/contact_.html'));
+        $this->assertFileDoesNotExist(base_path('public/static/about_.html'));
+    }
+
+    #[Test] public function it_clears_statically_cached_pages_with_a_wildcard()
+    {
+        $this->assertFileExists(base_path('public/static/contact_.html'));
+        $this->assertFileExists(base_path('public/static/contact/corporate-office_.html'));
+        $this->assertFileExists(base_path('public/static/contact/scranton-office_.html'));
+
+        $this
+            ->actingAs(User::make()->makeSuper()->save())
+            ->post(cp_route('utilities.static-cache-manager.clear'), [
+                'paths' => '/contact/*',
+            ]);
+
+        $this->assertFileExists(base_path('public/static/contact_.html'));
+        $this->assertFileDoesNotExist(base_path('public/static/contact/corporate-office_.html'));
+        $this->assertFileDoesNotExist(base_path('public/static/contact/scranton-office_.html'));
+    }
+
+    #[Test] public function it_clears_multiple_statically_cached_pages()
+    {
+        $this->assertFileExists(base_path('public/static/_.html'));
+        $this->assertFileExists(base_path('public/static/about_.html'));
+        $this->assertFileExists(base_path('public/static/contact_.html'));
+
+        $this
+            ->actingAs(User::make()->makeSuper()->save())
+            ->post(cp_route('utilities.static-cache-manager.clear'), [
+                'paths' => '/about' . PHP_EOL . '/contact',
+            ]);
+
+        $this->assertFileExists(base_path('public/static/_.html'));
+        $this->assertFileDoesNotExist(base_path('public/static/contact_.html'));
+        $this->assertFileDoesNotExist(base_path('public/static/about_.html'));
+    }
+}
