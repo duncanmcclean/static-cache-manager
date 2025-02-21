71c5ff2c5e2ed55f7ad243e0004ae3d1d7b84316
Duncan McClean <duncan@duncanmcclean.com>
2025-02-21 13:15:36 +0000

    Update GitHub Actions workflow (#27)
    * Update GitHub Actions workflow
    
    * Apply Code Style changes
    
    * Change minimum-stability to dev
    
    * Require a minimum version of Statamic
    
    * wip
    
    * Update the minimum version of Testbench
    
    * This is fine.
    
    ---------
    
    Co-authored-by: duncanmcclean <duncanmcclean@users.noreply.github.com>

--- tests/StaticCacheManagerTest.php
+++ tests/StaticCacheManagerTest.php
@@ -5,12 +5,12 @@ namespace DuncanMcClean\StaticCacheManager\Tests;
 use Illuminate\Support\Facades\File;
 use PHPUnit\Framework\Attributes\Test;
 use Statamic\Facades\Config;
-use Statamic\Facades\User;
 use Statamic\Facades\Site;
+use Statamic\Facades\User;
 
 class StaticCacheManagerTest extends TestCase
 {
-    public function setUp(): void
+    protected function setUp(): void
     {
         parent::setUp();
 
@@ -18,7 +18,7 @@ class StaticCacheManagerTest extends TestCase
         File::copyDirectory(__DIR__.'/__fixtures__/public/static-multisite', base_path('public/static-multisite'));
     }
 
-    public function tearDown(): void
+    protected function tearDown(): void
     {
         File::deleteDirectory(base_path('public/static'));
         File::deleteDirectory(base_path('public/static-multisite'));
@@ -26,7 +26,8 @@ class StaticCacheManagerTest extends TestCase
         parent::tearDown();
     }
 
-    #[Test] public function it_clears_a_statically_cached_page()
+    #[Test]
+    public function it_clears_a_statically_cached_page()
     {
         $this->assertFileExists(base_path('public/static/_.html'));
         $this->assertFileExists(base_path('public/static/about_.html'));
@@ -43,7 +44,8 @@ class StaticCacheManagerTest extends TestCase
         $this->assertFileDoesNotExist(base_path('public/static/about_.html'));
     }
 
-    #[Test] public function it_clears_statically_cached_pages_with_a_wildcard()
+    #[Test]
+    public function it_clears_statically_cached_pages_with_a_wildcard()
     {
         $this->assertFileExists(base_path('public/static/contact_.html'));
         $this->assertFileExists(base_path('public/static/contact/corporate-office_.html'));
@@ -60,7 +62,8 @@ class StaticCacheManagerTest extends TestCase
         $this->assertFileDoesNotExist(base_path('public/static/contact/scranton-office_.html'));
     }
 
-    #[Test] public function it_clears_multiple_statically_cached_pages()
+    #[Test]
+    public function it_clears_multiple_statically_cached_pages()
     {
         $this->assertFileExists(base_path('public/static/_.html'));
         $this->assertFileExists(base_path('public/static/about_.html'));
@@ -69,7 +72,7 @@ class StaticCacheManagerTest extends TestCase
         $this
             ->actingAs(User::make()->makeSuper()->save())
             ->post(cp_route('utilities.static-cache-manager.clear'), [
-                'paths' => '/about' . PHP_EOL . '/contact',
+                'paths' => '/about'.PHP_EOL.'/contact',
             ]);
 
         $this->assertFileExists(base_path('public/static/_.html'));
@@ -77,7 +80,8 @@ class StaticCacheManagerTest extends TestCase
         $this->assertFileDoesNotExist(base_path('public/static/about_.html'));
     }
 
-    #[Test] public function it_clears_a_statically_cached_page_in_a_multisite()
+    #[Test]
+    public function it_clears_a_statically_cached_page_in_a_multisite()
     {
         Site::setSites([
             [
