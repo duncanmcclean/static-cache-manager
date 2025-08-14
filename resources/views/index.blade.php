6b17d9be965e9909fde031d43f98aa3f683952fc
Duncan McClean <duncan@duncanmcclean.com>
2025-08-14 13:14:14 +0100

    [5.x] Statamic 6 (#29)
    * Require `dev-master` for the time being
    
    * Drop support for Laravel 10 and PHP 8.2
    
    * Update PHP version constraint in `composer.json`
    
    * Update the UI

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,22 +1,27 @@
+@php
+    use function Statamic\trans as __;
+@endphp
+
 @extends('statamic::layout')
 @section('title', __('Static Cache Manager'))
 
 @section('content')
-    <div class="flex items-center justify-between">
-        <h1>{{ __('Static Cache Manager') }}</h1>
-    </div>
+    <ui-header title="{{ __('Static Cache Manager') }}" icon="hammer-wrench"></ui-header>
 
-    <div class="mt-3 card">
-        <p class="mb-2">Which paths would you like to clear? One line per path.</p>
-
+    <ui-card-panel heading="{{ __('Invalidation') }}" subheading="{{ __('Which paths would you like to clear? One line per path.') }}">
         <form method="POST" action="{{ cp_route('utilities.static-cache-manager.clear') }}">
             @csrf
 
-            <textarea class="input-text mb-1 w-full lg:w-1/2" rows="6" name="paths" placeholder="Eg: blog/2020">{{ implode("\n", config('static-cache-manager.defaults.paths')) }}</textarea>
-
             <div>
-                <button type="submit" class="btn-primary">Clear</button>
+                <ui-textarea
+                    class="mb-4"
+                    rows="6"
+                    name="paths"
+                    placeholder="Eg: blog/2020"
+                    model-value="{{ implode("\n", config('static-cache-manager.defaults.paths')) }}"
+                ></ui-textarea>
+                <ui-button text="{{ __('Invalidate') }}" type="submit" variant="primary"></ui-button>
             </div>
         </form>
-    </div>
+    </ui-card-panel>
 @stop
